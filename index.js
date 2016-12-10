acb9acf3d74215f5337e833bf99389dd64a0c6b5
megos <gmegos.27@gmail.com>
2016-12-10 13:29:13 +0900

    add slack post

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const client = require('cheerio-httpcli');
+const client  = require('cheerio-httpcli');
+const request = require('request');
 
 const date = new Date().getTime();
 
+const slackWebhookUrl  = 'your webhook url';
+const slackBotUserName = 'usen-bot';
+
 const param = {
   npband: 'C',
-  npch: '26',
+  npch  : '26',
   nppage: 'yes',
-  _: date
+  _     : date
 };
 
 client.fetch('http://music.usen.com/usencms/search_nowplay1.php', param)
 .then((result) => {
-  console.log(result.$('.np-now li').text());
+  return result.$('.np-now li').text();
+})
+.then((np) => {
+  const options = {
+    url : slackWebhookUrl,
+    form: 'payload={"text": "' + np + '", "username": "' + slackBotUserName + '"}',
+    json: true
+  }
+  request.post(options, (error, res, body) => {
+    console.log(body);
+  });
 });
