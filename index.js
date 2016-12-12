1044c409b50600d80a5d4af8d326f195d7fca9c4
megos <gmegos.27@gmail.com>
2016-12-12 22:05:03 +0900

    create usen module

--- index.js
+++ index.js
@@ -1,46 +1,17 @@
-const client  = require('cheerio-httpcli');
-const request = require('request');
-const CronJob = require('cron').CronJob;
+'use strict'
 
-const slackWebhookUrl  = 'your slack url';
-const slackBotUserName = 'usen-bot';
+const client     = require('cheerio-httpcli');
+const CronJob    = require('cron').CronJob;
+const express    = require('express');
+const bodyParser = require('body-parser');
+const Usen       = require('./usen');
 
-const param = {
-  npband: 'C',
-  npch  : '26',
-  nppage: 'yes',
-  _     : ''
-};
-
-let nowPlaying = '';
-
-const postUsenNowPlaying = () => {
-  param.date = new Date().getTime();
-
-  client.fetch('http://music.usen.com/usencms/search_nowplay1.php', param)
-  .then((result) => {
-    return result.$('.np-now li').text().replace(/[ａ-ｚＡ-Ｚ０-９＝！＄＋＊％＆]/g, (s) => {
-      return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-    }).replace(/　/g, ' ');
-  })
-  .then((np) => {
-    if (np !== nowPlaying) {
-      const options = {
-        url : slackWebhookUrl,
-        form: 'payload={"text": "' + np + '", "username": "' + slackBotUserName + '"}',
-        json: true
-      };
-      request.post(options, (error, res, body) => {
-        console.log(body);
-        nowPlaying = np;
-      });
-    }
-  });
-}
+const usen = new Usen();
+const app  = express();
 
 const job = new CronJob({
   cronTime: '12 */1 * * * *',
-  onTick  : () => {postUsenNowPlaying()},
+  onTick  : () => {usen.postNowPlaying();},
   start   : true,
   timeZone: 'Asia/Tokyo'
 });
@@ -48,24 +19,16 @@ const job = new CronJob({
 job.start();
 
 
-const express    = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-
 app.use(bodyParser());
 app.listen(3000);
 
 app.post('/', (req, res) => {
-  param.npband = req.body.band.toUpperCase();
-  param.npch   = ('00' + req.body.ch).slice(-2);
+  usen.setBand(req.body.band.toUpperCase());
+  usen.setChannel(('00' + req.body.ch).slice(-2));
 
-  client.fetch('http://music.usen.com/channel/' + param.npband + param.npch + '/')
+  client.fetch('http://music.usen.com/channel/' + req.body.band.toLowerCase() + usen.getChannel() + '/')
   .then((result) => {
-    return result.$('.detail-title > h2').text();
-  })
-  .then((result) => {
-    postUsenNowPlaying();
-    res.send(result);
+    usen.postNowPlaying();
+    res.send(result.$('.detail-title > h2').text());
   });
 });
---
+++ usen.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const client  = require('cheerio-httpcli');
+const request = require('request');
+
+
+const Usen = function() {
+  this.param = {
+    npband: 'C',
+    npch  : '26',
+    nppage: 'yes',
+    _     : ''
+  };
+  this.nowPlaying = '';
+  this.botName    = 'usen-bot';
+  this.webhookUrl = '';
+};
+
+Usen.prototype = {
+
+  postNowPlaying: function() {
+
+    this.param._ = new Date().getTime();
+
+    client.fetch('http://music.usen.com/usencms/search_nowplay1.php', this.param)
+    .then((result) => {
+      return result.$('.np-now li').text().replace(/[ａ-ｚＡ-Ｚ０-９＝！＄＋＊％＆]/g, (s) => {
+        return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+      }).replace(/　/g, ' ');
+    })
+    .then((np) => {
+      if (np !== this.nowPlaying) {
+        const options = {
+          url : this.webhookUrl,
+          form: 'payload={"text": "' + np + '", "username": "' + this.botName + '"}',
+          json: true
+        };
+        request.post(options, (error, res, body) => {
+          console.log(body);
+          this.nowPlaying = np;
+        });
+      }
+    });
+  },
+
+  setBand: function(band) {
+    this.param.npband = band;
+  },
+
+  getBand: function() {
+    return this.param.npband;
+  },
+
+  setChannel: function(channel) {
+    this.param.npch = channel;
+  },
+
+  getChannel: function() {
+    return this.param.npch;
+  },
+
+  getChannelTitle: function() {
+    client.fetch('http://music.usen.com/channel/' + this.param.npband + this.param.npch + '/')
+    .then((result) => {
+      return result.$('.detail-title > h2').text();
+    });
+  }
+}
+
+module.exports = Usen;
