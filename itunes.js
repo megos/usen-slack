ae60376df89bb7be24a074ef09ac657df9e7fede
megos <gmegos.27@gmail.com>
2016-12-21 06:41:20 +0900

    add separate ja or us mmethod

--- itunes.js
+++ itunes.js
@@ -21,10 +21,15 @@ const itunesApi = {
   getArtworkUrl: function(songName, artistName) {
     this.setTerm(songName);
 
+    if (this.isHankaku(songName) && this.isHankaku(artistName)) {
+      options.qs.lang = 'en_us';
+    } else {
+      options.qs.lang = 'ja_jp';
+    }
+
     return new Promise((resolve, reject) => {
       request(options)
       .then((json) => {
-        console.log(json);
         if (json.results.length > 0 &&
           (json.results[0].artistName.toLowerCase() === artistName.toLowerCase() ||
             json.results[0].artistName.toLowerCase().replace(/and/g, '&') === artistName.toLowerCase())) {
@@ -42,6 +47,10 @@ const itunesApi = {
 
   setTerm: function(term) {
     options.qs.term = term;
+  },
+
+  isHankaku(term) {
+    return term.length === encodeURI(term).replace(/%[0-9A-F]{2}/g, 'L').length
   }
 }
 
