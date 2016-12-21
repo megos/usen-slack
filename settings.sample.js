cabb833ab54274bfce567f25a20c23aac6432f24
megos <gmegos.27@gmail.com>
2016-12-21 22:18:50 +0900

    change itunes api limit

--- settings.sample.js
+++ settings.sample.js
@@ -4,4 +4,4 @@ exports.EXPRESS_PORT = 3000;
 exports.CRON_TIME    = '00 */3 * * * *';
 exports.BOT_NAME     = 'usen-bot';
 exports.WEB_HOOK_URL = 'your slack incoming webhook url';
-exports.ITUNES_LIMIT = 10;
+exports.ITUNES_LIMIT = 1;
